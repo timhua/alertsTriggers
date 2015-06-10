c74b16255dc94080df917a796b3d10c7c1e2f13a
Tim Hua <tim@jolata.com>
2015-06-10 11:01:11 -0700

    express server loads index.html

---
+++ server/app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.static(__dirname + '/../public'));
+
+app.get('/', function(req,res){
+  res.send('index.html');
+  res.end();
+});
+
+app.listen(8000);
+
